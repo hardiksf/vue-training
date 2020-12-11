77ff2f024718a397eb19bce11f91ab9f90d49d37
Hardik Patel <emailhardikpatel@gmail.com>
2020-12-10 17:07:53 -0800

    Use of mapState Helper

--- vuex-app/src/components/CounterComponent.vue
+++ vuex-app/src/components/CounterComponent.vue
@@ -1,18 +1,40 @@
 <template>
   <div>
-    {{ count }}
+    <div>count: {{ count }}</div>
+    <div>countAlias: {{ countAlias }}</div>
+    <div>dataFromStatePlusFromLocal: {{ dataFromStatePlusFromLocal }}</div>
     <button @click="increment">+</button>
     <button @click="decrement">-</button>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
+
 export default {
   name: 'CounterComponent',
-  computed: {
-    count() {
-      return this.$store.state.count;
-    },
+  data() {
+    return {
+      localCount: 4,
+    };
   },
+  // computed: {
+  //   count() {
+  //     return this.$store.state.count;
+  //   },
+  // },
+  computed: mapState({
+    count: (state) => state.count,
+    countAlias: 'count',
+    dataFromStatePlusFromLocal(state) {
+      return state.count + this.localCount;
+    },
+  }),
+  // We can also pass a string array to mapState when the name of a mapped computed property is
+  // the same as a state sub tree name like below
+  // computed: mapState([
+  //   // map this.count to store.state.count
+  //   'count',
+  // ]),
   methods: {
     increment() {
       this.$store.commit('increment');
