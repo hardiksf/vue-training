c709a5713c6ee6fe684fb3342d7c5639098e3541
Hardik Patel <emailhardikpatel@gmail.com>
2021-05-14 12:56:38 -0700

    Game setup + attack

--- monster-slayer-game/src/components/Game.vue
+++ monster-slayer-game/src/components/Game.vue
@@ -1,5 +1,80 @@
 <template>
   <div>
-    from Game
+    <section class="health-container">
+      <h2>Monster Health</h2>
+      <div class="healthbar">
+          <div class="healthbar-value" :style="{width: monsterHealth + '%' }"></div>
+        </div>
+    </section>
+    <section class="health-container player">
+      <h2>Player Health</h2>
+       <div class="healthbar">
+          <div class="healthbar-value"></div>
+        </div>
+    </section>
+    <section class="control">
+        <button @click="attack">ATTACK</button>
+        <button>SPECIAL ATTACK</button>
+        <button>HEAL</button>
+        <button>SURRENDER</button>
+    </section>
   </div>
 </template>
+
+<script>
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+export default {
+
+  name: 'Game',
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100,
+    };
+  },
+  methods: {
+    attack() {
+      const attackValue = getRandomNumber(3, 8);
+      this.monsterHealth -= attackValue;
+    },
+  },
+};
+
+</script>
+
+<style>
+  .health-container {
+    border: 1px solid grey;
+    width: 50vw;
+    margin: 0 auto;
+  }
+  .healthbar {
+  width: 100%;
+  height: 4vw;
+  /* border: 1px solid #575757; */
+  margin: 1vw 0;
+  background: lightgrey;
+}
+
+.healthbar-value {
+  background-color: green;
+  /* width: 100%; */
+  height: 100%;
+}
+  .player {
+    margin-top: 5vw;
+    margin-bottom: 5vw;
+  }
+  .control {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .control button{
+    padding: 1vw;
+    margin: 1vw;
+    width: 25vw;
+  }
+</style>
