627a19704b17d654d0cd5f339a54bc8b64801e26
Hardik Patel <emailhardikpatel@gmail.com>
2022-07-20 07:29:18 -0700

    Adds results to UI & displays on mount as well

--- http-request/src/components/survey/UserExperiences.vue
+++ http-request/src/components/survey/UserExperiences.vue
@@ -32,15 +32,26 @@ export default {
   methods: {
     loadExperiences() {
       fetch("https://vue-http-demo-5562c-default-rtdb.firebaseio.com/surveys.json")
-      .then(function (response) { 
+      .then( (response) => { 
         if (response.ok) {
            return  response.json();
         }
        })
-       .then(function (data) {
-        console.log(data);
+       .then( (data) => {
+        const results = [];
+        for (const id in data) {
+          results.push({
+            id: id,
+            name: data[id].name,
+            rating: data[id].rating,
+          });
+        }
+        this.results = results;
        });
     },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
